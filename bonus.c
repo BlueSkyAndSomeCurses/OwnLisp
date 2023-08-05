8ce7b4689873410383297e0c0db160ee3b607881
Wouch <idontcare@noreply.xyz>
2023-08-05 15:14:57 +0300

    now polish notation functions as it has to, also realised bonus mark function to count leaves(i'm not sure whether it works correctly)

--- bonus.c
+++ bonus.c
@@ -27,6 +27,57 @@ void add_history(char* unused) {}
 // #include <editline/history.h>
 #endif
 
+/*
+typedef struct mpc_ast_t {
+  char* tag;
+  char* contents;
+  mpc_state_t state;
+  int children_num;
+  struct mpc_ast_t** children;
+} mpc_ast_t;
+*/
+long leaves(mpc_ast_t* t) {
+  if (t->children_num==0) return 1;
+  if (t->children_num>=1) {
+    long total = 1; 
+    for (int i = 0; i<t->children_num; ++i) {
+      total = total + leaves(t->children[i]);
+    }
+    return total;
+  }
+  return 0;
+}
+
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+")==0) return x+y;
+  if (strcmp(op, "-")==0) return x-y;
+  if (strcmp(op, "*")==0) return x*y;
+  if (strcmp(op, "/")==0) return x/y;
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  //If tagged as number return it directly
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  //The operator is always second child
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   // Create some parsers
@@ -34,20 +85,16 @@ int main(int argc, char** argv) {
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Lispatron = mpc_new("lispatron");
-  mpc_parser_t* Letter = mpc_new("letter");
-  mpc_parser_t* Word = mpc_new("word");
 
   //define them with the following language
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                     \ 
-      letter    :  /aba*/ | /a*ba*/ ;                               \
-      word      : /pit/ | /pot/ | /respite/ ;            \
-      number    : /-?[0-9]+/ | /-?[0-9]*[.][0-9]+/ ;                            \
-      operator  : '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/ ;                 \
-      expr      :  <number> | '(' <operator> <expr>+ ')' ; \
-      lispatron : /^/ <operator> <expr>+ | <letter>+ | <word> | <number> <operator> <number> | <operator> <number> /$/ ;            \
+    "                                                      \ 
+      number    : /-?[0-9]+/  ;                            \
+      operator  : '+' | '-' | '*' | '/' ;                  \
+      expr      : <number>  | '(' <operator> <expr>+ ')' ; \
+      lispatron : /^/ <operator> <expr>+ /$/ ;             \
     ",
-  Letter, Word, Number, Operator, Expr, Lispatron);
+   Number ,Operator, Expr, Lispatron);
 
 
   puts("lispatron version 0.0.1");
@@ -61,8 +108,14 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispatron, &r)) {
       // on success print AST
+      long result = eval(r.output);
+      long leaf = leaves(r.output);
+      printf("%li\n", result);
+      printf("Total amount of leaves is: %li\n", leaf);
+      
       mpc_ast_print(r.output);
       mpc_ast_delete(r.output);
+      
     } else {
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
@@ -71,7 +124,7 @@ int main(int argc, char** argv) {
     free(input);
   }
 
-  mpc_cleanup(4, Number, Operator, Expr, Lispatron, Letter, Word);
+  mpc_cleanup(4, Number, Operator, Expr, Lispatron);
   return 0;
 }
 
